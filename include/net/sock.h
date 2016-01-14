4e89a694adb15247c014d49eab7c82a09fccf0ab
Johannes Weiner <hannes@cmpxchg.org>
2016-01-14 15:21:02 -0800

    UPSTREAM: net: tcp_memcontrol: remove bogus hierarchy pressure propagation
    When a cgroup currently breaches its socket memory limit, it enters
    memory pressure mode for itself and its *ancestors*.  This throttles
    transmission in unrelated sibling and cousin subtrees that have nothing
    to do with the breached limit.
    
    On the contrary, breaching a limit should make that group and its
    *children* enter memory pressure mode.  But this happens already, albeit
    lazily: if an ancestor limit is breached, siblings will enter memory
    pressure on their own once the next packet arrives for them.
    
    So no additional hierarchy code is needed.  Remove the bogus stuff.
    
    Change-Id: I7997b5080b30ef6980b1cc6efcd15eb1c591603a
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: David S. Miller <davem@davemloft.net>
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1241,14 +1241,8 @@ static inline void sk_leave_memory_pressure(struct sock *sk)
 	if (*memory_pressure)
 		*memory_pressure = 0;
 
-	if (mem_cgroup_sockets_enabled && sk->sk_cgrp) {
-		struct cg_proto *cg_proto = sk->sk_cgrp;
-		struct proto *prot = sk->sk_prot;
-
-		for (; cg_proto; cg_proto = parent_cg_proto(prot, cg_proto))
-			cg_proto->memory_pressure = 0;
-	}
-
+	if (mem_cgroup_sockets_enabled && sk->sk_cgrp)
+		sk->sk_cgrp->memory_pressure = 0;
 }
 
 static inline void sk_enter_memory_pressure(struct sock *sk)
@@ -1256,13 +1250,8 @@ static inline void sk_enter_memory_pressure(struct sock *sk)
 	if (!sk->sk_prot->enter_memory_pressure)
 		return;
 
-	if (mem_cgroup_sockets_enabled && sk->sk_cgrp) {
-		struct cg_proto *cg_proto = sk->sk_cgrp;
-		struct proto *prot = sk->sk_prot;
-
-		for (; cg_proto; cg_proto = parent_cg_proto(prot, cg_proto))
-			cg_proto->memory_pressure = 1;
-	}
+	if (mem_cgroup_sockets_enabled && sk->sk_cgrp)
+		sk->sk_cgrp->memory_pressure = 1;
 
 	sk->sk_prot->enter_memory_pressure(sk);
 }
