5df7108156aaa556a5503aea8aa8ed4821d1625e
Al Viro <viro@zeniv.linux.org.uk>
2016-03-25 15:28:43 -0400

    constify security_path_chroot()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- security/security.c
+++ security/security.c
@@ -501,7 +501,7 @@ int security_path_chown(const struct path *path, kuid_t uid, kgid_t gid)
 }
 EXPORT_SYMBOL(security_path_chown);
 
-int security_path_chroot(struct path *path)
+int security_path_chroot(const struct path *path)
 {
 	return call_int_hook(path_chroot, 0, path);
 }
