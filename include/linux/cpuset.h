913af74b4480954c59871679fa714c5fe17154bd
Vlastimil Babka <vbabka@suse.cz>
2016-05-19 17:14:30 -0700

    cpuset: use static key better and convert to new API
    An important function for cpusets is cpuset_node_allowed(), which
    optimizes on the fact if there's a single root CPU set, it must be
    trivially allowed.  But the check "nr_cpusets() <= 1" doesn't use the
    cpusets_enabled_key static key the right way where static keys eliminate
    branching overhead with jump labels.
    
    This patch converts it so that static key is used properly.  It's also
    switched to the new static key API and the checking functions are
    converted to return bool instead of int.  We also provide a new variant
    __cpuset_zone_allowed() which expects that the static key check was
    already done and they key was enabled.  This is needed for
    get_page_from_freelist() where we want to also avoid the relatively
    slower check when ALLOC_CPUSET is not set in alloc_flags.
    
    The impact on the page allocator microbenchmark is less than expected
    but the cleanup in itself is worthwhile.
    
                                                 4.6.0-rc2                  4.6.0-rc2
                                           multcheck-v1r20               cpuset-v1r20
      Min      alloc-odr0-1               348.00 (  0.00%)           348.00 (  0.00%)
      Min      alloc-odr0-2               254.00 (  0.00%)           254.00 (  0.00%)
      Min      alloc-odr0-4               213.00 (  0.00%)           213.00 (  0.00%)
      Min      alloc-odr0-8               186.00 (  0.00%)           183.00 (  1.61%)
      Min      alloc-odr0-16              173.00 (  0.00%)           171.00 (  1.16%)
      Min      alloc-odr0-32              166.00 (  0.00%)           163.00 (  1.81%)
      Min      alloc-odr0-64              162.00 (  0.00%)           159.00 (  1.85%)
      Min      alloc-odr0-128             160.00 (  0.00%)           157.00 (  1.88%)
      Min      alloc-odr0-256             169.00 (  0.00%)           166.00 (  1.78%)
      Min      alloc-odr0-512             180.00 (  0.00%)           180.00 (  0.00%)
      Min      alloc-odr0-1024            188.00 (  0.00%)           187.00 (  0.53%)
      Min      alloc-odr0-2048            194.00 (  0.00%)           193.00 (  0.52%)
      Min      alloc-odr0-4096            199.00 (  0.00%)           198.00 (  0.50%)
      Min      alloc-odr0-8192            202.00 (  0.00%)           201.00 (  0.50%)
      Min      alloc-odr0-16384           203.00 (  0.00%)           202.00 (  0.49%)
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Acked-by: Zefan Li <lizefan@huawei.com>
    Cc: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/cpuset.h
+++ include/linux/cpuset.h
@@ -16,29 +16,41 @@
 
 #ifdef CONFIG_CPUSETS
 
-extern struct static_key cpusets_pre_enable_key;
-extern struct static_key cpusets_enabled_key;
+/*
+ * Static branch rewrites can happen in an arbitrary order for a given
+ * key. In code paths where we need to loop with read_mems_allowed_begin() and
+ * read_mems_allowed_retry() to get a consistent view of mems_allowed, we need
+ * to ensure that begin() always gets rewritten before retry() in the
+ * disabled -> enabled transition. If not, then if local irqs are disabled
+ * around the loop, we can deadlock since retry() would always be
+ * comparing the latest value of the mems_allowed seqcount against 0 as
+ * begin() still would see cpusets_enabled() as false. The enabled -> disabled
+ * transition should happen in reverse order for the same reasons (want to stop
+ * looking at real value of mems_allowed.sequence in retry() first).
+ */
+extern struct static_key_false cpusets_pre_enable_key;
+extern struct static_key_false cpusets_enabled_key;
 static inline bool cpusets_enabled(void)
 {
-	return static_key_false(&cpusets_enabled_key);
+	return static_branch_unlikely(&cpusets_enabled_key);
 }
 
 static inline int nr_cpusets(void)
 {
 	/* jump label reference count + the top-level cpuset */
-	return static_key_count(&cpusets_enabled_key) + 1;
+	return static_key_count(&cpusets_enabled_key.key) + 1;
 }
 
 static inline void cpuset_inc(void)
 {
-	static_key_slow_inc(&cpusets_pre_enable_key);
-	static_key_slow_inc(&cpusets_enabled_key);
+	static_branch_inc(&cpusets_pre_enable_key);
+	static_branch_inc(&cpusets_enabled_key);
 }
 
 static inline void cpuset_dec(void)
 {
-	static_key_slow_dec(&cpusets_enabled_key);
-	static_key_slow_dec(&cpusets_pre_enable_key);
+	static_branch_dec(&cpusets_enabled_key);
+	static_branch_dec(&cpusets_pre_enable_key);
 }
 
 extern int cpuset_init(void);
@@ -53,16 +65,25 @@ extern nodemask_t cpuset_mems_allowed(struct task_struct *p);
 void cpuset_init_current_mems_allowed(void);
 int cpuset_nodemask_valid_mems_allowed(nodemask_t *nodemask);
 
-extern int __cpuset_node_allowed(int node, gfp_t gfp_mask);
+extern bool __cpuset_node_allowed(int node, gfp_t gfp_mask);
 
-static inline int cpuset_node_allowed(int node, gfp_t gfp_mask)
+static inline bool cpuset_node_allowed(int node, gfp_t gfp_mask)
 {
-	return nr_cpusets() <= 1 || __cpuset_node_allowed(node, gfp_mask);
+	if (cpusets_enabled())
+		return __cpuset_node_allowed(node, gfp_mask);
+	return true;
 }
 
-static inline int cpuset_zone_allowed(struct zone *z, gfp_t gfp_mask)
+static inline bool __cpuset_zone_allowed(struct zone *z, gfp_t gfp_mask)
 {
-	return cpuset_node_allowed(zone_to_nid(z), gfp_mask);
+	return __cpuset_node_allowed(zone_to_nid(z), gfp_mask);
+}
+
+static inline bool cpuset_zone_allowed(struct zone *z, gfp_t gfp_mask)
+{
+	if (cpusets_enabled())
+		return __cpuset_zone_allowed(z, gfp_mask);
+	return true;
 }
 
 extern int cpuset_mems_allowed_intersects(const struct task_struct *tsk1,
@@ -109,7 +130,7 @@ extern void cpuset_print_current_mems_allowed(void);
  */
 static inline unsigned int read_mems_allowed_begin(void)
 {
-	if (!static_key_false(&cpusets_pre_enable_key))
+	if (!static_branch_unlikely(&cpusets_pre_enable_key))
 		return 0;
 
 	return read_seqcount_begin(&current->mems_allowed_seq);
@@ -123,7 +144,7 @@ static inline unsigned int read_mems_allowed_begin(void)
  */
 static inline bool read_mems_allowed_retry(unsigned int seq)
 {
-	if (!static_key_false(&cpusets_enabled_key))
+	if (!static_branch_unlikely(&cpusets_enabled_key))
 		return false;
 
 	return read_seqcount_retry(&current->mems_allowed_seq, seq);
@@ -181,14 +202,19 @@ static inline int cpuset_nodemask_valid_mems_allowed(nodemask_t *nodemask)
 	return 1;
 }
 
-static inline int cpuset_node_allowed(int node, gfp_t gfp_mask)
+static inline bool cpuset_node_allowed(int node, gfp_t gfp_mask)
 {
-	return 1;
+	return true;
 }
 
-static inline int cpuset_zone_allowed(struct zone *z, gfp_t gfp_mask)
+static inline bool __cpuset_zone_allowed(struct zone *z, gfp_t gfp_mask)
 {
-	return 1;
+	return true;
+}
+
+static inline bool cpuset_zone_allowed(struct zone *z, gfp_t gfp_mask)
+{
+	return true;
 }
 
 static inline int cpuset_mems_allowed_intersects(const struct task_struct *tsk1,
