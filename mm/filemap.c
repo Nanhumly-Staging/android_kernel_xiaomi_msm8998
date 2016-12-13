e3f530babcf658447f272fb067ff044fa74ea53e
Johannes Weiner <hannes@cmpxchg.org>
2016-12-12 16:43:55 -0800

    mm: workingset: restore refault tracking for single-page files
    Shadow entries in the page cache used to be accounted behind the radix
    tree implementation's back in the upper bits of node->count, and the
    radix tree code extending a single-entry tree with a shadow entry in
    root->rnode would corrupt that counter.  As a result, we could not put
    shadow entries at index 0 if the tree didn't have any other entries, and
    that means no refault detection for any single-page file.
    
    Now that the shadow entries are tracked natively in the radix tree's
    exceptional counter, this is no longer necessary.  Extending and
    shrinking the tree from and to single entries in root->rnode now does
    the right thing when the entry is exceptional, remove that limitation.
    
    [huexxx@gmail.com: adapt to available code]
    Link: http://lkml.kernel.org/r/20161117193244.GF23430@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Matthew Wilcox <mawilcox@linuxonhyperv.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -152,14 +152,6 @@ static void page_cache_tree_delete(struct address_space *mapping,
 	__radix_tree_lookup(&mapping->page_tree, page->index,
 				    &node, &slot);
 
-	if (!node) {
-		/*
-		 * We need a node to properly account shadow
-		 * entries. Don't plant any without. XXX
-		 */
-		shadow = NULL;
-	}
-
 	radix_tree_clear_tags(&mapping->page_tree, node, slot);
 	__radix_tree_replace(&mapping->page_tree, node, slot, shadow,
 			     workingset_update_node, mapping);
