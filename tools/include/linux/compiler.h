f1e731fd6b525bbc5cdd2c3235d5cbd70384b081
Matthew Wilcox <mawilcox@microsoft.com>
2016-12-16 14:53:45 -0500

    radix tree test suite: Remove types.h
    Move the pieces we still need to tools/include and update a few implicit
    includes.
    
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>

--- tools/include/linux/compiler.h
+++ tools/include/linux/compiler.h
@@ -10,6 +10,8 @@
 #endif
 
 #define __user
+#define __rcu
+#define __read_mostly
 
 #ifndef __attribute_const__
 # define __attribute_const__
@@ -38,6 +40,8 @@
 #ifndef unlikely
 # define unlikely(x)		__builtin_expect(!!(x), 0)
 #endif
+
+#define uninitialized_var(x) x = *(&(x))
 
 #define ACCESS_ONCE(x) (*(volatile typeof(x) *)&(x))
 
