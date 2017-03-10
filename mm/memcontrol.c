f3bc4b6a078e870aef47476e60e0db017037d9d2
Tahsin Erdogan <tahsin@google.com>
2017-03-09 16:17:26 -0800

    UPSTREAM: mm: do not call mem_cgroup_free() from within mem_cgroup_alloc()
    mem_cgroup_free() indirectly calls wb_domain_exit() which is not
    prepared to deal with a struct wb_domain object that hasn't executed
    wb_domain_init().  For instance, the following warning message is
    printed by lockdep if alloc_percpu() fails in mem_cgroup_alloc():
    
      INFO: trying to register non-static key.
      the code is fine but needs lockdep annotation.
      turning off the locking correctness validator.
      CPU: 1 PID: 1950 Comm: mkdir Not tainted 4.10.0+ #151
      Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS Bochs 01/01/2011
      Call Trace:
       dump_stack+0x67/0x99
       register_lock_class+0x36d/0x540
       __lock_acquire+0x7f/0x1a30
       lock_acquire+0xcc/0x200
       del_timer_sync+0x3c/0xc0
       wb_domain_exit+0x14/0x20
       mem_cgroup_free+0x14/0x40
       mem_cgroup_css_alloc+0x3f9/0x620
       cgroup_apply_control_enable+0x190/0x390
       cgroup_mkdir+0x290/0x3d0
       kernfs_iop_mkdir+0x58/0x80
       vfs_mkdir+0x10e/0x1a0
       SyS_mkdirat+0xa8/0xd0
       SyS_mkdir+0x14/0x20
       entry_SYSCALL_64_fastpath+0x18/0xad
    
    Add __mem_cgroup_free() which skips wb_domain_exit().  This is used by
    both mem_cgroup_free() and mem_cgroup_alloc() clean up.
    
    Fixes: 0b8f73e104285 ("mm: memcontrol: clean up alloc, online, offline, free functions")
    Link: http://lkml.kernel.org/r/20170306192122.24262-1-tahsin@google.com
    Change-Id: I3e029d84d7a56b19f1556dbb2617b10659473b79
    Signed-off-by: Tahsin Erdogan <tahsin@google.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4238,15 +4238,20 @@ static void free_mem_cgroup_per_zone_info(struct mem_cgroup *memcg, int node)
 	kfree(memcg->nodeinfo[node]);
 }
 
-static void mem_cgroup_free(struct mem_cgroup *memcg)
+static void __mem_cgroup_free(struct mem_cgroup *memcg)
 {
 	int node;
 
-	memcg_wb_domain_exit(memcg);
 	for_each_node(node)
 		free_mem_cgroup_per_zone_info(memcg, node);
 	free_percpu(memcg->stat);
 	kfree(memcg);
+}
+
+static void mem_cgroup_free(struct mem_cgroup *memcg)
+{
+	memcg_wb_domain_exit(memcg);
+	__mem_cgroup_free(memcg);
 }
 
 static struct mem_cgroup *mem_cgroup_alloc(void)
@@ -4357,7 +4362,7 @@ out:
 	return &memcg->css;
 fail:
 	idr_remove(&mem_cgroup_idr, memcg->id.id);
-	mem_cgroup_free(memcg);
+	__mem_cgroup_free(memcg);
 	return ERR_PTR(error);
 }
 
