5c6ce86ca6265cbfa36a5ea1127baeadadcd2d40
Paolo Abeni <pabeni@redhat.com>
2017-04-24 14:18:28 +0200

    ipv6: move stub initialization after ipv6 setup completion
    The ipv6 stub pointer is currently initialized before the ipv6
    routing subsystem: a 3rd party can access and use such stub
    before the routing data is ready.
    Moreover, such pointer is not cleared in case of initialization
    error, possibly leading to dangling pointers usage.
    
    This change addresses the above moving the stub initialization
    at the end of ipv6 init code.
    
    Fixes: 5f81bd2e5d80 ("ipv6: export a stub for IPv6 symbols used by vxlan")
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Acked-by: Cong Wang <xiyou.wangcong@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/af_inet6.c
+++ net/ipv6/af_inet6.c
@@ -964,8 +964,6 @@ static int __init inet6_init(void)
 	if (err)
 		goto igmp_fail;
 
-	ipv6_stub = &ipv6_stub_impl;
-
 	err = ipv6_netfilter_init();
 	if (err)
 		goto netfilter_fail;
@@ -1029,6 +1027,10 @@ static int __init inet6_init(void)
 	if (err)
 		goto sysctl_fail;
 #endif
+
+	/* ensure that ipv6 stubs are visible only after ipv6 is ready */
+	wmb();
+	ipv6_stub = &ipv6_stub_impl;
 out:
 	return err;
 
