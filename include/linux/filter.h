dfcc7b1b896ebd34bda22b0dc847cda2153f1fac
Jesper Dangaard Brouer <brouer@redhat.com>
2018-02-13 14:15:36 +0100

    UPSTREAM: net: avoid including xdp.h in filter.h
    If is sufficient with a forward declaration of struct xdp_rxq_info in
    linux/filter.h, which avoids including net/xdp.h.  This was originally
    suggested by John Fastabend during the review phase, but wasn't
    included in the final patchset revision.  Thus, this followup.
    
    Suggested-by: John Fastabend <john.fastabend@gmail.com>
    Change-Id: I6753b3463017a5bc99ac8907e4c992289549af21
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -17,7 +17,6 @@
 #include <linux/cryptohash.h>
 #include <linux/kallsyms.h>
 
-#include <net/xdp.h>
 #include <net/sch_generic.h>
 
 #include <asm/byteorder.h>
@@ -30,6 +29,7 @@ struct sk_buff;
 struct sock;
 struct seccomp_data;
 struct bpf_prog_aux;
+struct xdp_rxq_info;
 struct ctl_table;
 struct ctl_table_header;
 struct sock_reuseport;
