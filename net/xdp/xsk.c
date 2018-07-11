bd8cacdc2b726894ae4a3d8ad5ac65c74d066899
Magnus Karlsson <magnus.karlsson@intel.com>
2018-07-11 10:12:50 +0200

    UPSTREAM: xsk: do not return EAGAIN from sendmsg when completion queue is full
    This patch stops returning EAGAIN in TX copy mode when the completion
    queue is full as zero-copy does not do this. Instead this situation
    can be detected by comparing the head and tail pointers of the
    completion queue in both modes. In any case, EAGAIN was not the
    correct error code here since no amount of calling sendmsg will solve
    the problem. Only consuming one or more messages on the completion
    queue will fix this.
    
    With this patch, the error reporting becomes consistent between copy
    mode and zero-copy mode.
    
    Fixes: 35fcde7f8deb ("xsk: support for Tx")
    Change-Id: I284e336915ccfb3430c7ddc734bf30b6e4c7106f
    Signed-off-by: Magnus Karlsson <magnus.karlsson@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/xdp/xsk.c
+++ net/xdp/xsk.c
@@ -240,10 +240,8 @@ static int xsk_generic_xmit(struct sock *sk, struct msghdr *m,
 			goto out;
 		}
 
-		if (xskq_reserve_addr(xs->umem->cq)) {
-			err = -EAGAIN;
+		if (xskq_reserve_addr(xs->umem->cq))
 			goto out;
-		}
 
 		len = desc.len;
 		if (unlikely(len > xs->dev->mtu)) {
