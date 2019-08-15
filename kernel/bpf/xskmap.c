13d56013d1bec0861d7d0ace9e37cb1b2dde90d1
Björn Töpel <bjorn.topel@intel.com>
2019-08-15 11:30:14 +0200

    UPSTREAM: xsk: support BPF_EXIST and BPF_NOEXIST flags in XSKMAP
    The XSKMAP did not honor the BPF_EXIST/BPF_NOEXIST flags when updating
    an entry. This patch addresses that.
    
    Change-Id: I405bec641507812949781a95965821abc3aa50d5
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/xskmap.c
+++ kernel/bpf/xskmap.c
@@ -234,8 +234,6 @@ static int xsk_map_update_elem(struct bpf_map *map, void *key, void *value,
 		return -EINVAL;
 	if (unlikely(i >= m->map.max_entries))
 		return -E2BIG;
-	if (unlikely(map_flags == BPF_NOEXIST))
-		return -EEXIST;
 
 	sock = sockfd_lookup(fd, &err);
 	if (!sock)
@@ -264,6 +262,12 @@ static int xsk_map_update_elem(struct bpf_map *map, void *key, void *value,
 	old_xs = READ_ONCE(*map_entry);
 	if (old_xs == xs) {
 		err = 0;
+		goto out;
+	} else if (old_xs && map_flags == BPF_NOEXIST) {
+		err = -EEXIST;
+		goto out;
+	} else if (!old_xs && map_flags == BPF_EXIST) {
+		err = -ENOENT;
 		goto out;
 	}
 	xsk_map_sock_add(xs, node);
