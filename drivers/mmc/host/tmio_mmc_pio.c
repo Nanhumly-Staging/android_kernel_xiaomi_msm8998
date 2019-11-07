a3de7b949006807aff41ac0e471dff8f689bf299
Ben Hutchings <ben.hutchings@codethink.co.uk>
2019-11-07 08:31:39 +0000

    mmc: tmio, sh_mobile_sdhi: Pass tmio_mmc_host ptr to clk_{enable, disable} ops
    commit 0ea28210c15680bbabc3d6079f771f1a1e69509a upstream.
    
    Change the clk_enable operation to take a pointer to the struct
    tmio_mmc_host and have it set f_max.  For consistency, also change the
    clk_disable operation to take a pointer to struct tmio_mmc_host.
    
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Biju Das <biju.das@bp.renesas.com>
    Signed-off-by: Pavel Machek <pavel@denx.de>

--- drivers/mmc/host/tmio_mmc_pio.c
+++ drivers/mmc/host/tmio_mmc_pio.c
@@ -843,7 +843,7 @@ static int tmio_mmc_clk_update(struct tmio_mmc_host *host)
 	if (!host->clk_enable)
 		return -ENOTSUPP;
 
-	ret = host->clk_enable(host->pdev, &mmc->f_max);
+	ret = host->clk_enable(host);
 	if (!ret)
 		mmc->f_min = mmc->f_max / 512;
 
@@ -1249,7 +1249,7 @@ int tmio_mmc_host_runtime_suspend(struct device *dev)
 		tmio_mmc_clk_stop(host);
 
 	if (host->clk_disable)
-		host->clk_disable(host->pdev);
+		host->clk_disable(host);
 
 	return 0;
 }
