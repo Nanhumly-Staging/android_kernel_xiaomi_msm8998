cb692b7cb5615199b15df0fe6cb78f7a442a5829
Chris Brandt <chris.brandt@renesas.com>
2019-11-19 13:35:40 +0000

    mmc: tmio-mmc: add support for 32bit data port
    commit 8185e51f358a8dd4801b67e8c66f03eb9eeaba75 upstream.
    
    For the r7s72100 SOC, the DATA_PORT register was changed to 32-bits wide.
    Therefore a new flag has been created that will allow 32-bit reads/writes
    to the DATA_PORT register instead of 16-bit (because 16-bits accesses are
    not supported).
    
    Signed-off-by: Chris Brandt <chris.brandt@renesas.com>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    [biju: backported the upstream commit 9c284c41c0886f09e75c323a16278b6d353b0b4a]
    Signed-off-by: Biju Das <biju.das@bp.renesas.com>
    Signed-off-by: Pavel Machek <pavel@denx.de>

--- drivers/mmc/host/tmio_mmc_pio.c
+++ drivers/mmc/host/tmio_mmc_pio.c
@@ -393,6 +393,35 @@ static void tmio_mmc_transfer_data(struct tmio_mmc_host *host,
 	/*
 	 * Transfer the data
 	 */
+	if (host->pdata->flags & TMIO_MMC_32BIT_DATA_PORT) {
+		u32 data = 0;
+		u32 *buf32 = (u32 *)buf;
+
+		if (is_read)
+			sd_ctrl_read32_rep(host, CTL_SD_DATA_PORT, buf32,
+					   count >> 2);
+		else
+			sd_ctrl_write32_rep(host, CTL_SD_DATA_PORT, buf32,
+					    count >> 2);
+
+		/* if count was multiple of 4 */
+		if (!(count & 0x3))
+			return;
+
+		buf32 += count >> 2;
+		count %= 4;
+
+		if (is_read) {
+			sd_ctrl_read32_rep(host, CTL_SD_DATA_PORT, &data, 1);
+			memcpy(buf32, &data, count);
+		} else {
+			memcpy(&data, buf32, count);
+			sd_ctrl_write32_rep(host, CTL_SD_DATA_PORT, &data, 1);
+		}
+
+		return;
+	}
+
 	if (is_read)
 		sd_ctrl_read16_rep(host, CTL_SD_DATA_PORT, buf, count >> 1);
 	else
