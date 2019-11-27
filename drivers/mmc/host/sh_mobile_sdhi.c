ca2805d6de165c00b63720d1acca0f1e2ae8576f
Wolfram Sang <wsa+renesas@sang-engineering.com>
2019-11-27 13:46:42 +0000

    mmc: sh_mobile_sdhi: enable SDIO IRQs for RCar Gen3
    commit 685d29ef1783af0049c4aeeec43722e410d5845d upstream.
    
    Tested on a Salvator-X board with a Spectec SDW-823 WLAN card.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Biju Das <biju.das@bp.renesas.com>
    Signed-off-by: Pavel Machek <pavel@denx.de>

--- drivers/mmc/host/sh_mobile_sdhi.c
+++ drivers/mmc/host/sh_mobile_sdhi.c
@@ -109,7 +109,7 @@ static struct sh_mobile_sdhi_scc rcar_gen3_scc_taps[] = {
 static const struct sh_mobile_sdhi_of_data of_rcar_gen3_compatible = {
 	.tmio_flags	= TMIO_MMC_HAS_IDLE_WAIT | TMIO_MMC_WRPROTECT_DISABLE |
 			  TMIO_MMC_CLK_ACTUAL | TMIO_MMC_MIN_RCAR2,
-	.capabilities	= MMC_CAP_SD_HIGHSPEED,
+	.capabilities	= MMC_CAP_SD_HIGHSPEED | MMC_CAP_SDIO_IRQ,
 	.bus_shift	= 2,
 	.scc_offset	= 0x1000,
 	.taps		= rcar_gen3_scc_taps,
