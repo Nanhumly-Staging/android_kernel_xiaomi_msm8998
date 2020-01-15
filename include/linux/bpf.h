8f7f2d6baf57da5d4eee955ae704b373780c42b6
Brian Vazquez <brianvv@google.com>
2020-01-15 10:43:02 -0800

    UPSTREAM: bpf: Add generic support for update and delete batch ops
    This commit adds generic support for update and delete batch ops that
    can be used for almost all the bpf maps. These commands share the same
    UAPI attr that lookup and lookup_and_delete batch ops use and the
    syscall commands are:
    
      BPF_MAP_UPDATE_BATCH
      BPF_MAP_DELETE_BATCH
    
    The main difference between update/delete and lookup batch ops is that
    for update/delete keys/values must be specified for userspace and
    because of that, neither in_batch nor out_batch are used.
    
    Suggested-by: Stanislav Fomichev <sdf@google.com>
    Change-Id: I88478f807c59a39ea19f4a7d9108ceee7d154207
    Signed-off-by: Brian Vazquez <brianvv@google.com>
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200115184308.162644-4-brianvv@google.com

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -54,6 +54,10 @@ struct bpf_map_ops {
 	void *(*map_lookup_elem_sys_only)(struct bpf_map *map, void *key);
 	int (*map_lookup_batch)(struct bpf_map *map, const union bpf_attr *attr,
 				union bpf_attr __user *uattr);
+	int (*map_update_batch)(struct bpf_map *map, const union bpf_attr *attr,
+				union bpf_attr __user *uattr);
+	int (*map_delete_batch)(struct bpf_map *map, const union bpf_attr *attr,
+				union bpf_attr __user *uattr);
 
 	/* funcs callable from userspace and from eBPF programs */
 	void *(*map_lookup_elem)(struct bpf_map *map, void *key);
@@ -825,6 +829,12 @@ void bpf_map_area_free(void *base);
 void bpf_map_init_from_attr(struct bpf_map *map, union bpf_attr *attr);
 struct bpf_map *bpf_map_get_curr_or_next(u32 *id);
 int generic_map_lookup_batch(struct bpf_map *map,
+			     const union bpf_attr *attr,
+			     union bpf_attr __user *uattr);
+int generic_map_update_batch(struct bpf_map *map,
+			     const union bpf_attr *attr,
+			     union bpf_attr __user *uattr);
+int generic_map_delete_batch(struct bpf_map *map,
 			     const union bpf_attr *attr,
 			     union bpf_attr __user *uattr);
 
