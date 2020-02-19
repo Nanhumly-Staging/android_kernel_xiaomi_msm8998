23885c1e59f4f31d74d1e95c6164a4ba0cdb0f5e
Alistair Delva <adelva@google.com>
2020-02-18 22:47:34 -0800

    add init-check

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -300,18 +300,14 @@ static void msm_restart_prepare(const char *cmd)
 #endif
 
 	/* Hard reset the PMIC unless memory contents must be maintained. */
-	if (need_warm_reset) {
+	if (true) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
 	} else {
-		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
+		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);	
 	}
 
 	if (cmd != NULL) {
-		if (!strncmp(cmd, "bootloader", 10)) {
-			qpnp_pon_set_restart_reason(
-				PON_RESTART_REASON_BOOTLOADER);
-			__raw_writel(0x77665500, restart_reason);
-		} else if (!strncmp(cmd, "recovery", 8)) {
+		if (!strncmp(cmd, "bootloader", 10) || !strncmp(cmd, "recovery", 8)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_RECOVERY);
 			__raw_writel(0x77665502, restart_reason);
