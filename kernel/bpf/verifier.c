bbd31d7aac0f42861b80b717910305b6cc77ad3c
KP Singh <kpsingh@google.com>
2020-03-05 21:49:55 +0100

    UPSTREAM: bpf: Remove unnecessary CAP_MAC_ADMIN check
    While well intentioned, checking CAP_MAC_ADMIN for attaching
    BPF_MODIFY_RETURN tracing programs to "security_" functions is not
    necessary as tracing BPF programs already require CAP_SYS_ADMIN.
    
    Fixes: 6ba43b761c41 ("bpf: Attachment verification for BPF_MODIFY_RETURN")
    Change-Id: I742a7ce23a2a17d0902d1ea1da852e3f344b3cb9
    Signed-off-by: KP Singh <kpsingh@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200305204955.31123-1-kpsingh@chromium.org

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -10220,20 +10220,14 @@ static int check_attach_modify_return(struct bpf_verifier_env *env)
 	struct bpf_prog *prog = env->prog;
 	unsigned long addr = (unsigned long)prog->aux->trampoline->func.addr;
 
-	if (within_error_injection_list(addr))
-		return 0;
-
 	/*
 	 * Security hooks are allowed until the LSM_HOOK conversion can
 	 * provide a dedicated BTF-based allow-list.
 	 */
-	if (!strncmp(SECURITY_PREFIX, prog->aux->attach_func_name,
-		     sizeof(SECURITY_PREFIX) - 1)) {
-		if (!capable(CAP_MAC_ADMIN))
-			return -EPERM;
-
+	if (within_error_injection_list(addr) ||
+	    !strncmp(SECURITY_PREFIX, prog->aux->attach_func_name,
+		     sizeof(SECURITY_PREFIX) - 1))
 		return 0;
-	}
 
 	verbose(env, "fmod_ret attach_btf_id %u (%s) is not modifiable\n",
 		prog->aux->attach_btf_id, prog->aux->attach_func_name);
