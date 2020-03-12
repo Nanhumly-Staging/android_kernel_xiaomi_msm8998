3c5109175ed1eda10da1c8b7746498fadee0fcc8
Jiri Olsa <jolsa@kernel.org>
2020-03-12 20:56:00 +0100

    UPSTREAM: bpf: Move lnode list node to struct bpf_ksym
    Adding lnode list node to 'struct bpf_ksym' object,
    so the struct bpf_ksym itself can be chained and used
    in other objects like bpf_trampoline and bpf_dispatcher.
    
    Changing iterator to bpf_ksym in bpf_get_kallsym function.
    
    The ksym->start is holding the prog->bpf_func value,
    so it's ok to use it as value in bpf_get_kallsym.
    
    Change-Id: Id4a14d9fa23126379f2ee9e390c705751f6021ae
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200312195610.346362-6-jolsa@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -490,6 +490,7 @@ struct bpf_ksym {
 	unsigned long		 start;
 	unsigned long		 end;
 	char			name[KSYM_NAME_LEN];
+	struct list_head		lnode;
 };
 
 enum bpf_tramp_prog_type {
@@ -583,7 +584,6 @@ struct bpf_prog_aux {
 	struct bpf_prog **func;
 	void *jit_data; /* JIT specific data. arch dependent */
 	struct latch_tree_node ksym_tnode;
-	struct list_head ksym_lnode;
 	struct bpf_ksym ksym;
 	const struct bpf_prog_ops *ops;
 	struct bpf_map **used_maps;
