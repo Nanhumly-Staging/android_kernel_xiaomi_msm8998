a877e2ff93ecfabf72c5b7488a1d1a2b29c68480
Jiri Olsa <jolsa@kernel.org>
2020-07-11 23:53:23 +0200

    UPSTREAM: bpf: Add BTF_ID_LIST/BTF_ID/BTF_ID_UNUSED macros
    Adding support to generate .BTF_ids section that will hold BTF
    ID lists for verifier.
    
    Adding macros that will help to define lists of BTF ID values
    placed in .BTF_ids section. They are initially filled with zeros
    (during compilation) and resolved later during the linking phase
    by resolve_btfids tool.
    
    Following defines list of one BTF ID value:
    
      BTF_ID_LIST(bpf_skb_output_btf_ids)
      BTF_ID(struct, sk_buff)
    
    It also defines following variable to access the list:
    
      extern u32 bpf_skb_output_btf_ids[];
    
    The BTF_ID_UNUSED macro defines 4 zero bytes. It's used when we
    want to define 'unused' entry in BTF_ID_LIST, like:
    
      BTF_ID_LIST(bpf_skb_output_btf_ids)
      BTF_ID(struct, sk_buff)
      BTF_ID_UNUSED
      BTF_ID(struct, task_struct)
    
    Suggested-by: Andrii Nakryiko <andriin@fb.com>
    Change-Id: Iaa217d59fd32ee61400f597adb8c89c4242b28ad
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Tested-by: Andrii Nakryiko <andriin@fb.com>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200711215329.41165-4-jolsa@kernel.org

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -554,6 +554,10 @@
 		VMLINUX_SYMBOL(__start_BTF) = .;			\
 		*(.BTF)							\
 		VMLINUX_SYMBOL(__stop_BTF) = .;			        \
+	}								\
+	. = ALIGN(4);							\
+	.BTF_ids : AT(ADDR(.BTF_ids) - LOAD_OFFSET) {			\
+		*(.BTF_ids)						\
 	}
 #else
 #define BTF
