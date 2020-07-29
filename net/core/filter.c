b50c80a794cb066ce88e5a0705cf9d820a99beac
Stanislav Fomichev <sdf@google.com>
2020-07-28 17:31:03 -0700

    UPSTREAM: bpf: Expose socket storage to BPF_PROG_TYPE_CGROUP_SOCK
    This lets us use socket storage from the following hooks:
    
    * BPF_CGROUP_INET_SOCK_CREATE
    * BPF_CGROUP_INET_SOCK_RELEASE
    * BPF_CGROUP_INET4_POST_BIND
    * BPF_CGROUP_INET6_POST_BIND
    
    Using existing 'bpf_sk_storage_get_proto' doesn't work because
    second argument is ARG_PTR_TO_SOCKET. Even though
    BPF_PROG_TYPE_CGROUP_SOCK hooks operate on 'struct bpf_sock',
    the verifier still considers it as a PTR_TO_CTX.
    That's why I'm adding another 'bpf_sk_storage_get_cg_sock_proto'
    definition strictly for BPF_PROG_TYPE_CGROUP_SOCK which accepts
    ARG_PTR_TO_CTX which is really 'struct sock' for this program type.
    
    Change-Id: I32bda1b9eb1f30067f58b7acb4d6140c70becdbc
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200729003104.1280813-1-sdf@google.com

--- net/core/filter.c
+++ net/core/filter.c
@@ -4472,6 +4472,7 @@ static const struct bpf_func_proto bpf_sock_ops_cb_flags_set_proto = {
 };
 
 const struct bpf_func_proto bpf_event_output_data_proto __weak;
+const struct bpf_func_proto bpf_sk_storage_get_cg_sock_proto __weak;
 
 static const struct bpf_func_proto *
 sock_filter_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
@@ -4504,6 +4505,8 @@ sock_filter_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 	case BPF_FUNC_get_cgroup_classid:
 		return &bpf_get_cgroup_classid_curr_proto;
 #endif
+	case BPF_FUNC_sk_storage_get:
+		return &bpf_sk_storage_get_cg_sock_proto;
 	default:
 		return bpf_base_func_proto(func_id, prog);
 	}
