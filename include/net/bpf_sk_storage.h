507685c7a7033d17362b226ef6935149948f4df9
KP Singh <kpsingh@google.com>
2020-08-25 20:29:14 +0200

    UPSTREAM: bpf: Generalize caching for sk_storage.
    Provide the a ability to define local storage caches on a per-object
    type basis. The caches and caching indices for different objects should
    not be inter-mixed as suggested in:
    
      https://lore.kernel.org/bpf/20200630193441.kdwnkestulg5erii@kafai-mbp.dhcp.thefacebook.com/
    
      "Caching a sk-storage at idx=0 of a sk should not stop an
      inode-storage to be cached at the same idx of a inode."
    
    Change-Id: Ifa2f4618ba00061dfe3242bf069f0ba3bfed25bd
    Signed-off-by: KP Singh <kpsingh@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200825182919.1118197-3-kpsingh@chromium.org

--- include/net/bpf_sk_storage.h
+++ include/net/bpf_sk_storage.h
@@ -3,12 +3,31 @@
 #ifndef _BPF_SK_STORAGE_H
 #define _BPF_SK_STORAGE_H
 
+#include <linux/types.h>
+#include <linux/spinlock.h>
+
 struct sock;
 
 void bpf_sk_storage_free(struct sock *sk);
 
 extern const struct bpf_func_proto bpf_sk_storage_get_proto;
 extern const struct bpf_func_proto bpf_sk_storage_delete_proto;
+
+#define BPF_LOCAL_STORAGE_CACHE_SIZE	16
+
+struct bpf_local_storage_cache {
+	spinlock_t idx_lock;
+	u64 idx_usage_counts[BPF_LOCAL_STORAGE_CACHE_SIZE];
+};
+
+#define DEFINE_BPF_STORAGE_CACHE(name)				\
+static struct bpf_local_storage_cache name = {			\
+	.idx_lock = __SPIN_LOCK_UNLOCKED(name.idx_lock),	\
+}
+
+u16 bpf_local_storage_cache_idx_get(struct bpf_local_storage_cache *cache);
+void bpf_local_storage_cache_idx_free(struct bpf_local_storage_cache *cache,
+				      u16 idx);
 
 #ifdef CONFIG_BPF_SYSCALL
 int bpf_sk_storage_clone(const struct sock *sk, struct sock *newsk);
