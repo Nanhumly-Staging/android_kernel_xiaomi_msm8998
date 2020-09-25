90c4255eb0701ef9c63bcd70b3379cf514ead064
Martin KaFai Lau <kafai@fb.com>
2020-09-24 17:04:09 -0700

    UPSTREAM: bpf: Change bpf_tcp_*_syncookie to accept ARG_PTR_TO_BTF_ID_SOCK_COMMON
    This patch changes the bpf_tcp_*_syncookie() to take
    ARG_PTR_TO_BTF_ID_SOCK_COMMON such that they will work with the pointer
    returned by the bpf_skc_to_*() helpers also.
    
    Change-Id: I4f7712acada428dab29b94f0cb9bbbb19d9eaea2
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Lorenz Bauer <lmb@cloudflare.com>
    Link: https://lore.kernel.org/bpf/20200925000409.3856725-1-kafai@fb.com

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -1633,7 +1633,7 @@ union bpf_attr {
  *
  *		**-EOVERFLOW** if an overflow happened: The same object will be tried again.
  *
- * int bpf_tcp_check_syncookie(struct bpf_sock *sk, void *iph, u32 iph_len, struct tcphdr *th, u32 th_len)
+ * long bpf_tcp_check_syncookie(void *sk, void *iph, u32 iph_len, struct tcphdr *th, u32 th_len)
  * 	Description
  * 		Check whether iph and th contain a valid SYN cookie ACK for
  * 		the listening socket in sk.
@@ -1648,7 +1648,7 @@ union bpf_attr {
  * 		0 if iph and th are a valid SYN cookie ACK, or a negative error
  * 		otherwise.
  *
- * s64 bpf_tcp_gen_syncookie(struct bpf_sock *sk, void *iph, u32 iph_len, struct tcphdr *th, u32 th_len)
+ * s64 bpf_tcp_gen_syncookie(void *sk, void *iph, u32 iph_len, struct tcphdr *th, u32 th_len)
  *	Description
  *		Try to issue a SYN cookie for the packet with corresponding
  *		IP/TCP headers, *iph* and *th*, on the listening socket in *sk*.
