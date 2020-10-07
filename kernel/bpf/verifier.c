562c84bdd3461557217891f12bc21cb675554f7f
Randy Dunlap <rdunlap@infradead.org>
2020-10-06 19:16:13 -0700

    UPSTREAM: kernel/bpf/verifier: Fix build when NET is not enabled
    Fix build errors in kernel/bpf/verifier.c when CONFIG_NET is
    not enabled.
    
    ../kernel/bpf/verifier.c:3995:13: error: ‘btf_sock_ids’ undeclared here (not in a function); did you mean ‘bpf_sock_ops’?
      .btf_id = &btf_sock_ids[BTF_SOCK_TYPE_SOCK_COMMON],
    
    ../kernel/bpf/verifier.c:3995:26: error: ‘BTF_SOCK_TYPE_SOCK_COMMON’ undeclared here (not in a function); did you mean ‘PTR_TO_SOCK_COMMON’?
      .btf_id = &btf_sock_ids[BTF_SOCK_TYPE_SOCK_COMMON],
    
    Fixes: 1df8f55a37bd ("bpf: Enable bpf_skc_to_* sock casting helper to networking prog type")
    Change-Id: I032cabe53d99923cfbfe9af33f19d1c1ba1e57d0
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/20201007021613.13646-1-rdunlap@infradead.org

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3555,6 +3555,7 @@ static const struct bpf_reg_types sock_types = {
 	},
 };
 
+#ifdef CONFIG_NET
 static const struct bpf_reg_types btf_id_sock_common_types = {
 	.types = {
 		PTR_TO_SOCK_COMMON,
@@ -3564,6 +3565,7 @@ static const struct bpf_reg_types btf_id_sock_common_types = {
 	},
 	.btf_id = &btf_sock_ids[BTF_SOCK_TYPE_SOCK_COMMON],
 };
+#endif
 
 static const struct bpf_reg_types mem_types = {
 	.types = {
@@ -3604,7 +3606,9 @@ static const struct bpf_reg_types *compatible_reg_types[__BPF_ARG_TYPE_MAX] = {
 	[ARG_PTR_TO_CTX]		= &context_types,
 	[ARG_PTR_TO_CTX_OR_NULL]	= &context_types,
 	[ARG_PTR_TO_SOCK_COMMON]	= &sock_types,
+#ifdef CONFIG_NET
 	[ARG_PTR_TO_BTF_ID_SOCK_COMMON]	= &btf_id_sock_common_types,
+#endif
 	[ARG_PTR_TO_SOCKET]		= &fullsock_types,
 	[ARG_PTR_TO_BTF_ID]		= &btf_ptr_types,
 	[ARG_PTR_TO_SPIN_LOCK]		= &spin_lock_types,
