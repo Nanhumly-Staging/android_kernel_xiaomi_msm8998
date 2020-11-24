55b7aac3de4850c48d4ca1486619f8742d8bd4c6
Biju Das <biju.das.jz@bp.renesas.com>
2020-11-24 17:09:29 +0000

    usb: host: xhci-plat: Add r8a7742 support
    This patch adds xhci r8a7742 driver support.
    
    There are lot of changes between upstream and 4.4 kernel and
    we cannot cleanly backport the changes from upstream.
    
    This patch is similar to commit d09ccbb73386 ("usb: host:
    xhci-plat: Add r8a7743 support") present in cip kernel.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro1.iwamatsu@toshiba.co.jp>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -54,6 +54,7 @@ static int xhci_plat_setup(struct usb_hcd *hcd)
 
 	if (of_device_is_compatible(of_node, "renesas,xhci-r8a7790") ||
 	    of_device_is_compatible(of_node, "renesas,xhci-r8a7791") ||
+	    of_device_is_compatible(of_node, "renesas,xhci-r8a7742") ||
 	    of_device_is_compatible(of_node, "renesas,xhci-r8a7743") ||
 	    of_device_is_compatible(of_node, "renesas,xhci-r8a7744")) {
 		ret = xhci_rcar_init_quirk(hcd);
@@ -70,6 +71,7 @@ static int xhci_plat_start(struct usb_hcd *hcd)
 
 	if (of_device_is_compatible(of_node, "renesas,xhci-r8a7790") ||
 	    of_device_is_compatible(of_node, "renesas,xhci-r8a7791") ||
+	    of_device_is_compatible(of_node, "renesas,xhci-r8a7742") ||
 	    of_device_is_compatible(of_node, "renesas,xhci-r8a7743") ||
 	    of_device_is_compatible(of_node, "renesas,xhci-r8a7744"))
 		xhci_rcar_start(hcd);
@@ -271,6 +273,7 @@ static const struct of_device_id usb_xhci_of_match[] = {
 	{ .compatible = "xhci-platform" },
 	{ .compatible = "marvell,armada-375-xhci"},
 	{ .compatible = "marvell,armada-380-xhci"},
+	{ .compatible = "renesas,xhci-r8a7742"},
 	{ .compatible = "renesas,xhci-r8a7743"},
 	{ .compatible = "renesas,xhci-r8a7744"},
 	{ .compatible = "renesas,xhci-r8a7790"},
