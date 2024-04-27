aa58a7a35d33058c710ae1e185cb0e9774b7c28e
AShiningRay <genesis45.ppc@gmail.com>
2024-04-27 12:40:38 -0300

    drivers: usb: gadget: revert f_fs.c to its state prior to CIP merge
    It is now back to SHA 2def68c11eb5cdd0a10068b80237d36cc793101d.
    
    This checkout, along with one more file on USB, is able to fix
    USB as a whole on LGE_8996. Given how much code is shared with
    other platforms in these affected files, it'll probably help
    xiaomi, google, etc. as well.
    
    Change-Id: I3f454ab57ba540c41d19b694c351960a21d6c710

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -243,11 +243,6 @@ static int __ffs_ep0_queue_wait(struct ffs_data *ffs, char *data, size_t len)
 	struct usb_request *req = ffs->ep0req;
 	int ret;
 
-	if (!req) {
-		spin_unlock_irq(&ffs->ev.waitq.lock);
-		return -EINVAL;
-	}
-
 	req->zero     = len < le16_to_cpu(ffs->ev.setup.wLength);
 
 	spin_unlock_irq(&ffs->ev.waitq.lock);
@@ -1956,9 +1951,6 @@ static void functionfs_unbind(struct ffs_data *ffs)
 	ENTER();
 
 	if (!WARN_ON(!ffs->gadget)) {
-		/* dequeue before freeing ep0req */
-		usb_ep_dequeue(ffs->gadget->ep0, ffs->ep0req);
-		mutex_lock(&ffs->mutex);
 		usb_ep_free_request(ffs->gadget->ep0, ffs->ep0req);
 		ffs->ep0req = NULL;
 		ffs->gadget = NULL;
@@ -3980,7 +3972,6 @@ static void ffs_func_unbind(struct usb_configuration *c,
 		ffs->func = NULL;
 	}
 
-	ffs_event_add(ffs, FUNCTIONFS_UNBIND);
 	if (!--opts->refcnt)
 		functionfs_unbind(ffs);
 
