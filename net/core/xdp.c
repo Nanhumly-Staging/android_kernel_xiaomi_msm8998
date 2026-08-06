22df1cc6f42d5b23254b04c7eae512367c1104f8
juezhi6668 <1649417471@qq.com>
2026-08-06 23:19:51 +0800

    Revert "BACKPORT: xdp: transition into using xdp_frame for return API"
    This reverts commit 59abc670b47ce416a90a55d9f4100a9bec06348c.

--- net/core/xdp.c
+++ net/core/xdp.c
@@ -312,11 +312,9 @@ err:
 }
 EXPORT_SYMBOL_GPL(xdp_rxq_info_reg_mem_model);
 
-void xdp_return_frame(struct xdp_frame *xdpf)
+void xdp_return_frame(void *data, struct xdp_mem_info *mem)
 {
-	struct xdp_mem_info *mem = &xdpf->mem;
 	struct xdp_mem_allocator *xa;
-	void *data = xdpf->data;
 	struct page *page;
 
 	switch (mem->type) {
