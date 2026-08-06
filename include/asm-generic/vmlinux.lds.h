b3676f118386a5fe36a4072c690df46f4dba4894
juezhi6668 <1649417471@qq.com>
2026-08-06 23:18:11 +0800

    Revert "BACKPORT: bpf: Add BTF_ID_LIST/BTF_ID/BTF_ID_UNUSED macros"
    This reverts commit e89fe73733004ece335766d7063c67de1c73e8ae.

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -553,11 +553,7 @@
 	.BTF : AT(ADDR(.BTF) - LOAD_OFFSET) {		                \
 		VMLINUX_SYMBOL(__start_BTF) = .;			\
 		*(.BTF)							\
-		VMLINUX_SYMBOL(__stop_BTF) = .;						\
-	}								\
-	. = ALIGN(4);							\
-	.BTF_ids : AT(ADDR(.BTF_ids) - LOAD_OFFSET) {			\
-		*(.BTF_ids)						\
+		VMLINUX_SYMBOL(__stop_BTF) = .;			        \
 	}
 #else
 #define BTF
