af062189234b2c6605ceaaf06e59f861663f86fa
juezhi6668 <1649417471@qq.com>
2026-08-06 23:20:18 +0800

    Revert "UPSTREAM: bpf: cpumap fix potential lost wake-up problem"
    This reverts commit 373c5806a6f7b98fe26248ed8458fcd68ba2243e.

--- kernel/bpf/cpumap.c
+++ kernel/bpf/cpumap.c
@@ -288,17 +288,13 @@ static int cpu_map_kthread_run(void *data)
 
 		/* Release CPU reschedule checks */
 		if (__ptr_ring_empty(rcpu->queue)) {
-			set_current_state(TASK_INTERRUPTIBLE);
-			/* Recheck to avoid lost wake-up */
-			if (__ptr_ring_empty(rcpu->queue)) {
-				schedule();
-				sched = 1;
-			} else {
-				__set_current_state(TASK_RUNNING);
-			}
+			__set_current_state(TASK_INTERRUPTIBLE);
+			schedule();
+			sched = 1;
 		} else {
 			sched = cond_resched();
 		}
+		__set_current_state(TASK_RUNNING);
 
 		/* Process packets in rcpu->queue */
 		local_bh_disable();
